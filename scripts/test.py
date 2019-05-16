28348d5c641b9bd2bd4d1e5d3b5c877453e84459
muke123com <muke123com@163.com>
2019-05-16 22:48:21 +0800

    xpath

--- scripts/test.py
+++ scripts/test.py
@@ -1,23 +1,47 @@
 import requests
 import json
+import lxml.html
+import pymysql
+etree = lxml.html.etree
 
-url = "https://graphql.epicgames.com/graphql"
+url = ""
 
-payload = "{\n    \"query\": \"\\n          query promotionsQuery($namespace: String!, $country: String!) {\\n            Catalog {\\n              catalogOffers(namespace: $namespace, params: {category: \\\"freegames\\\", country: $country, sortBy: \\\"effectiveDate\\\", sortDir: \\\"asc\\\"}) {\\n                elements {\\n                  title\\n                  description\\n                  id\\n                  namespace\\n                  linkedOfferNs\\n                  linkedOfferId\\n                  keyImages {\\n                    type\\n                    url\\n                  }\\n                  productSlug\\n                  promotions {\\n                    promotionalOffers {\\n                      promotionalOffers {\\n                        startDate\\n                        endDate\\n                        discountSetting {\\n                          discountType\\n                          discountPercentage\\n                        }\\n                      }\\n                    }\\n                    upcomingPromotionalOffers {\\n                      promotionalOffers {\\n                        startDate\\n                        endDate\\n                        discountSetting {\\n                          discountType\\n                          discountPercentage\\n                        }\\n                      }\\n                    }\\n                  }\\n                }\\n              }\\n            }\\n          }\\n        \",\n    \"variables\": {\n        \"namespace\": \"epic\",\n        \"country\": \"US\"\n    }\n}"
+payload = {
+    'mode':'async',
+    'function':'get_block',
+    'block_id':'list_videos_videos_list_search_result',
+    'q':'',
+    'category_ids':'',
+    'sort_by':'',
+    'from_videos':'02',
+    'from_albums':'02',
+    '_':1558014199821
+}
 headers = {
     'Content-Type': "application/json",
-    # 'User-Agent': "PostmanRuntime/7.11.0",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36 115Browser/9.1.1",
     # 'Accept': "*/*",
     # 'Cache-Control': "no-cache",
     # 'Postman-Token': "e7e7eb75-0fef-4a28-8d3b-235c67370432,635be8fe-9bde-4810-b9ad-06438e8372dd",
-    'Host': "graphql.epicgames.com",
+    'Host': "www.femdomtb.com",
     # 'accept-encoding': "gzip, deflate",
     # 'content-length': "1531",
     # 'Connection': "keep-alive",
     # 'cache-control': "no-cache"
     }
 
-r = requests.post(url, data=payload, headers=headers);
+r = requests.get(url, data=payload, headers=headers);
 r.encoding = "utf-8"
-res = json.loads(r.text);
-print(res);
+# res = json.loads(r.text);
+# el = '<div class="margin-fix" id="list_videos_recommended_videos_items">23213123</div>';
+element = etree.HTML(r.text)
+title_list = element.xpath('//div[@id="list_videos_recommended_videos_items"]//a/@title')
+href_list = element.xpath('//div[@id="list_videos_recommended_videos_items"]//a/@href')
+img_list = element.xpath('//div[@id="list_videos_recommended_videos_items"]//a//img/@data-original')
+# result = etree.tostring(text[0], method='text')
+for title in img_list:
+    print(title);
+    pass;
+print ('result is: ', len(img_list));
+# print(r.text);
+
