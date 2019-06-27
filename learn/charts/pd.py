8ac23292d5532dc18d3ba876f534f97c23b01863
muke123com <muke123com@163.com>
2019-06-27 19:39:18 +0800

    pd

--- learn/charts/pd.py
+++ learn/charts/pd.py
@@ -1,14 +1,47 @@
 import pandas as pd
+import numpy as np
 
-movie_info = pd.read_csv("./tmdb_5000_movies.csv")
+df = pd.DataFrame({"id": [1001, 1002, 1003, 1004, 1005, 1006],
+                   "date": pd.date_range('20130102', periods=6),  # !
+                   "city": ['Beijing ', 'SH', ' guangzhou ', 'Shenzhen', 'shanghai', 'BEIJING '],
+                   "age": [23, 44, 54, 32, 34, 32],
+                   "category": ['100-A', '100-B', '110-A', '110-C', '210-A', '130-F'],
+                   "price": [1200, np.nan, 2133, 5433, np.nan, 4432]},  # !
+                  columns=['id', 'date', 'city', 'category', 'age', 'price'])
 
-print(movie_info.head(3))
-print(movie_info.shape)
-print(movie_info.loc[0])
+df1 = pd.DataFrame({"id": [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008],
+                    "gender": ['male', 'female', 'male', 'female', 'male', 'female', 'male', 'female'],
+                    "pay": ['Y', 'N', 'Y', 'Y', 'N', 'Y', 'N', 'Y', ],
+                    "m-point": [10, 12, 20, 40, 40, 40, 30, 20]})
 
-columns = ["title", "spoken_languages"]
-m_list = movie_info[columns]
-print(m_list.loc[0:100])
-print(pd.isnull(movie_info['title']))
+print(df.shape)
+print(df.dtypes)
+print(df.info())
 
+print(df.isnull())
+print(df.values)
 
+df_inner = pd.merge(df, df1, how='inner')  # 匹配合并，交集
+df_left = pd.merge(df, df1, how='left')  #
+df_right = pd.merge(df, df1, how='right')
+df_outer = pd.merge(df, df1, how='outer')  # 并集
+
+result = df.append(df1)
+# 设置索引列
+df_inner.set_index('id')
+# 按照特定列的值排序
+df_inner.sort_values(by=['age'])
+# 按照索引列排序
+df_inner.sort_index()
+# 如果prince列的值>3000，group列显示high，否则显示low
+df_inner['group'] = np.where(df_inner['price'] > 3000, 'high', 'low')
+# 对复合多个条件的数据进行分组标记
+df_inner.loc[(df_inner['city'] == 'Shenzhen') & (df_inner['price'] <= 4000), 'sign'] = 1
+# 对category字段的值依次进行分列，并创建数据表，索引值为df_inner的索引列，列名称为category和size
+split = pd.DataFrame((x.split('-') for x in df_inner['category']), index=df_inner.index, columns=['category-s', 'size'])
+df_inner = pd.merge(df_inner, split, right_index=True, left_index=True)
+
+# 数据提取
+# 主要用到的三个函数：loc,iloc和ix，loc函数按标签值进行提取，iloc按位置进行提取，ix可以同时按标签和位置进行提取。
+
+pass
---
+++ libs/pixiv.py
@@ -0,0 +1,135 @@
+# https://www.pixiv.net/
+
+import requests
+import json
+import time
+import os
+import lxml.html
+import pymysql
+etree = lxml.html.etree
+
+class Pic(object):
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+        }
+        self.count = 0
+        self.post_data = {}
+        self.str = ''
+        self.data_url = ''
+        self.pic_base = ''
+        self.files_folder = 'D:/PycharmProjects/images/'
+        self.session = requests.session()
+        self.search_key = ""
+
+        self.db = pymysql.connect(host='localhost', user='root', password='123456', port=3306, db='m_learn')
+        self.cursor = self.db.cursor()
+
+        pass
+
+    def get_pics(self):
+        if self.count > 5:
+            return
+        key = self.search_key
+        key = key.encode(encoding='utf-8')
+        self.post_data['query'] = key
+        # self.post_data['cursor'] = ''
+        post_data = self.post_data
+        res = self.session.get(self.data_url, params=post_data, headers=self.headers)
+        res = json.loads(res.text)
+        data_list = res['data']
+        sql_data = []
+        for item in data_list:
+            sql_item = {}
+            sql_item['p_keyword'] = key
+            sql_item['p_key'] = item['key']
+            tags = item['tags']
+            tags_str = ','.join(tags)
+            sql_item['p_tags'] = tags_str
+            sql_item['p_favorite'] = 0
+            sql_item['p_like'] = 0
+            if item.get('reactions'):
+                sql_item['p_favorite'] = item['reactions'].get('favorite')
+                sql_item['p_like'] = item['reactions'].get('like')
+
+            if item['contentType'] == "image/jpeg":
+                s = item['transforms']['']
+            else:
+                s = item['transforms'][':image/jpeg']
+
+            sql_item['p_url'] = s
+
+            sql_data.append(sql_item)
+
+        self.save_pics(sql_data)
+        if len(sql_data) < 50:
+            return
+        self.post_data['cursor'] = res['cursor']
+
+        self.get_pics()
+
+        pass
+
+    def save_pics(self, sql_data):
+        table = 'm_pic'
+        data = sql_data[0]
+        keys = ', '.join(data.keys())
+        values = ','.join(['%s'] * len(data))
+        sql = 'INSERT INTO {table}({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE'.format(table=table, keys=keys,
+                                                                                             values=values)
+        update = ', '.join([" {key} = VALUES({key})".format(key=key) for key in data])
+        sql += update
+
+        tuple_list = []
+        for data in sql_data:
+            tuple_list.append(tuple(data.values()))
+        try:
+            self.cursor.executemany(sql, tuple_list)
+            self.db.commit()
+            print(len(sql_data))
+        except Exception as e:
+            print(e)
+            self.db.rollback()
+        pass
+
+    def download_pics(self):
+        sql = "SELECT p_url FROM m_pic WHERE p_tags LIKE '%boot%'"
+        try:
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            print('dddddd')
+        except Exception as e:
+            print(e)
+            self.db.rollback()
+        o_count = 0
+        d_count = 0
+        all_count = len(result)
+        for l in result:
+            l = "".join(l)
+            pic_url = self.pic_base + l
+            path = self.files_folder
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            has_file = os.path.exists(path + l + '.jpg')
+            if not has_file:
+                try:
+                    r = requests.get(pic_url)
+                    d_count += 1
+
+                except requests.exceptions.Timeout:
+                    print('timeout')
+                    r = requests.get(pic_url)
+                    d_count += 1
+
+                with open(path + l + '.jpg', 'wb') as file:
+                    file.write(r.content)
+                    print('新下载数：' + str(d_count) + ' 总数：' + str(d_count + o_count) + '/' + str(all_count))
+            else:
+                o_count += 1
+    pass
+
+if __name__ == '__main__':
+    p = Pic()
+    p.get_pics()
+    # p.download_pics()
