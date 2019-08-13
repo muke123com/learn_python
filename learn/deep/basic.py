baaa186bcb251fba7a9b148d567492f9cdc869bd
muke123com <muke123com@163.com>
2019-08-13 17:39:47 +0800

    mnist

--- learn/deep/basic.py
+++ learn/deep/basic.py
@@ -1,27 +1,16 @@
 import tensorflow as tf
+# 创建一个常量op
+m1 = tf.constant([[3, 3]])
+# 创建一个常量op
+m2 = tf.constant([[2], [3]])
+# 创建矩阵乘法op
+product = tf.matmul(m1, m2)
 
+# 定义会话
 sess = tf.compat.v1.Session()
-t1 = tf.constant(10)
-t1_r = sess.run(t1)
-print(t1_r)
 
-t2 = tf.constant([[2, 2], [2, 2]])
-t2_r = sess.run(t2)
-print(t2_r)
+result = sess.run(product)
 
-t3 = tf.Variable(0, name="counter")
-t3_one = tf.constant(1)
-t3_new_value = tf.add(t3, t3_one)
-t3_update = tf.compat.v1.assign(t3, t3_new_value)
-
-t3_init_op = tf.compat.v1.global_variables_initializer()
-sess.run(t3_init_op)
-print(sess.run(t3))
-
-for _ in range(3):
-    sess.run(t3_update)
-    print(sess.run(t3))
-
-
-
+print(result)
 sess.close()
+
